e1cc6c8fd7743dd4af01717aa12ac5cf94944888
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-10-03 16:25:58 -0300

    09 - Ranges

---
+++ ranges/range.py
@@ -0,0 +1,15 @@
+# Declarando um range
+range_1 = range(5)
+
+range_2 = range(2, 10)
+
+range_3 = range(2, 10, 2)
+
+# for i in range_1:
+#     print(i)
+#
+# for i in range_2:
+#     print(i)
+
+for i in range_3:
+    print(i)
